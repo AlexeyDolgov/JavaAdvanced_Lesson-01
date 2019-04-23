b656f46f60c978ba5fa763fc72ea96e2a14afd66
Alexey Dolgov <bifourcator@gmail.com>
2019-04-23 22:33:05 +0300

    Java Advanced Lesson 1 done!

---
+++ JavaAdvanced_Lesson-01/src/test/java/ua/lviv/lgs/task24/AllTests.java
@@ -0,0 +1,10 @@
+package ua.lviv.lgs.task24;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TimeTest.class, ScheduleTest.class })
+
+public class AllTests { }
